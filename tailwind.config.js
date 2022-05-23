fda00104e819ef8c6a7153086a4a329674b8421a
hamza <hamzah@paritie.com>
2022-05-23 06:33:16 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./index.html"],
+  theme: {
+    extend: {
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+        xl: "1440px",
+      }, //screen ends here
+      colors: {
+        primaryBlue: "#3E4095",
+        gold: "#F59134",
+        paleOrange: "hsl(25, 100%, 94%)",
+        grayishBlue: "hsl(220, 14%, 75%)",
+        lightGrayishBlue: "hsl(223, 64%, 98%)",
+      }, //color ends here
+    }, //extend ends here
+  }, //theme ends here
+  plugins: [],
+};
